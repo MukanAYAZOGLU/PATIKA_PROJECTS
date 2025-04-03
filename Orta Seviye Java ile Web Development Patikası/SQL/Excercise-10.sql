5959779c2ff4c20ca579514a1f2e709def180f5e
MUKANAYAZOĞLU <postaturkey@yaani.com>
2025-04-03 17:11:45 +0300

    tenth sql exercise uploaded (changed).

--- Orta Seviye Java ile Web Development Patikası/SQL/Excercise-10.sql
+++ Orta Seviye Java ile Web Development Patikası/SQL/Excercise-10.sql
@@ -4,8 +4,8 @@ select city, country from city left join country on city.name = country.city_nam
 
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
 
-select payment_id, first_name, last_name from customer right join customer on payment.payment_id = customer.id;
+select first_name, last_name from customer right join customer on payment.payment_id = customer.id;
 
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
 
-select rental_id, first_name, last_name from customer full join rental on rental.rentalid = customer.id;
+select first_name, last_name from customer full join rental on rental.rentalid = customer.id;
